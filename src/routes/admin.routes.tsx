37c0550527b7a3b41a1b966c0cea77524ea27533
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-01-07 13:42:53 +0600

    Routing using Programmatic way (using reduce)

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,22 +3,84 @@ import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 
-// ! Hard coded way
 export const adminPaths = [
   {
+    index: true,
+    element: <AdminDashboard />,
+  },
+  {
+    name: "Dashboard",
     path: "dashboard",
     element: <AdminDashboard />,
   },
   {
-    path: "create-admin",
-    element: <CreateAdmin />,
-  },
-  {
-    path: "create-faculty",
-    element: <CreateFaculty />,
-  },
-  {
-    path: "create-student",
-    element: <CreateStudent />,
+    name: "User Management",
+    children: [
+      {
+        name: "Dashboard",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+    ],
   },
 ];
+
+// Programmatic way
+type TAdminRoute = {
+  path?: string;
+  element: JSX.Element;
+  index?: boolean;
+};
+export const adminRoutes = adminPaths.reduce<TAdminRoute[]>((acc, item) => {
+  if (item.index && item.element) {
+    acc.push({
+      index: item.index,
+      element: item.element,
+    });
+  }
+  if (item.name && item.element && item.path) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+// ! Hard coded way
+// export const adminRoutes = [
+//   {
+//     path: "dashboard",
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin />,
+//   },
+//   {
+//     path: "create-faculty",
+//     element: <CreateFaculty />,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent />,
+//   },
+// ];
---
+++ test.js
@@ -0,0 +1,56 @@
+export const adminPaths = [
+  {
+    index: true,
+    element: "AdminDashboard",
+  },
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: "AdminDashboard",
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Dashboard",
+        path: "create-admin",
+        element: "CreateAdmin",
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: "CreateFaculty",
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: "CreateStudent",
+      },
+    ],
+  },
+];
+const adminRouts = adminPaths.reduce((acc, item) => {
+  if (item.index && item.element) {
+    acc.push({
+      index: item.index,
+      element: item.element,
+    });
+  }
+  if (item.name && item.element && item.path) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+console.log(adminRouts);
