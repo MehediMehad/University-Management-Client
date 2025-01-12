20cf834394228a838e5c20a9f6ef80f4cc776690
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-01-12 10:50:41 +0600

    Create sidebar component

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { Menu } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.routes";
+
+const Sidebar = () => {
+  return (
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div
+        style={{
+          color: "white",
+          textAlign: "center",
+          height: "4rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>GAI</h1>
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={sidebarItemsGenerator(adminPaths, "admin")}
+      />
+    </Sider>
+  );
+};
+
+export default Sidebar;
